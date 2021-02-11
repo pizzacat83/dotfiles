de1be71fcba2081c65f084e80da94726565679bf
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-02-11 22:16:29 +0900

    fish: add scripts for setting up tools

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,11 @@ set FAKE_HOSTNAME (random choice kotatsu ofuton hidamari ohiza)
 set FAKE_USERNAME pizzacat83
 
 # set general variables
+
+# setup tools
+# rust
+set -x PATH "$HOME/.cargo/bin" $PATH
+# opam
+test -d ~/.opam && source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+# vscode
+set -x PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $PATH
