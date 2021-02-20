11a6ade3b5a9214441e2552b1d71a15ff7f984bf
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-02-20 13:32:02 +0900

    macos-defaults: add --dry-run option to install.sh

--- macos-defaults/install.sh
+++ macos-defaults/install.sh
@@ -1,6 +1,30 @@
 #!/bin/sh -eu
 set -eu
 
+if [ "$#" -ne 1 ]; then
+    echo "[ERROR] This script accepts exactly one argument."
+    # shellcheck disable=SC2016
+    echo 'Run `install.sh --dry-run` if you want to check what will be installed.'
+    # shellcheck disable=SC2016
+    echo 'Run `install.sh -y` if you really want to install.'
+    exit 1
+fi
+
+case "${1}" in
+    "--dry-run")
+        echo "[INFO] Dry run mode. Files are not actually created."
+        dry_run=1
+        ;;
+    "-y" | "--yes")
+        echo "[INFO] Installation mode."
+        dry_run=0
+        ;;
+    *)
+        echo "[ERROR] Unknown argument: ${1}"
+        exit 1
+        ;;
+esac
+
 if [ "$(uname)" != "Darwin" ]; then
     echo "[SKIP] This script only works on macOS."
     exit
@@ -9,11 +33,13 @@ fi
 # Set screenshot save location
 
 if defaults read com.apple.screencapture location > /dev/null; then
-    screenshot_dir="$HOME/Pictures/screenshots"
-    if [ -d "$screenshot_dir" ]; then
-        mkdir -p "$screenshot_dir"
+    if [ "$dry_run" -eq 0 ]; then
+        screenshot_dir="$HOME/Pictures/screenshots"
+        if [ -d "$screenshot_dir" ]; then
+            mkdir -p "$screenshot_dir"
+        fi
+        defaults write com.apple.screencapture location "$screenshot_dir"
     fi
-    defaults write com.apple.screencapture location "$screenshot_dir"
 else
     echo "[ERROR] Failed to set screenshot save location."
     # shellcheck disable=SC2016
