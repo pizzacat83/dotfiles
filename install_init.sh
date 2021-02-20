447856e5a5a63f709b4eb29aad04978bcb1ac053
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-02-21 07:10:09 +0900

    install_init.sh: extract common init scripts

---
+++ install_init.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -eu
+set -eu
+
+# source this file in each install.sh
+
+if [ "$#" -ne 1 ]; then
+    echo "[ERROR] This script accepts exactly one argument."
+    # shellcheck disable=SC2016
+    echo 'Run `install.sh --dry-run` if you want to check what will be installed.'
+    # shellcheck disable=SC2016
+    echo 'Run `install.sh -y` if you really want to install.'
+    exit 1
+fi
+
+case "${1}" in
+    "--dry-run")
+        echo "[INFO] Dry run mode. Files are not actually created."
+        dry_run=1
+        ;;
+    "-y" | "--yes")
+        echo "[INFO] Installation mode."
+        # shellcheck disable=SC2034
+        dry_run=0
+        ;;
+    *)
+        echo "[ERROR] Unknown argument: ${1}"
+        exit 1
+        ;;
+esac
--- macos-defaults/install.sh
+++ macos-defaults/install.sh
@@ -1,29 +1,9 @@
 #!/bin/sh -eu
 set -eu
+cd "$(dirname "$0")"
 
-if [ "$#" -ne 1 ]; then
-    echo "[ERROR] This script accepts exactly one argument."
-    # shellcheck disable=SC2016
-    echo 'Run `install.sh --dry-run` if you want to check what will be installed.'
-    # shellcheck disable=SC2016
-    echo 'Run `install.sh -y` if you really want to install.'
-    exit 1
-fi
-
-case "${1}" in
-    "--dry-run")
-        echo "[INFO] Dry run mode. Files are not actually created."
-        dry_run=1
-        ;;
-    "-y" | "--yes")
-        echo "[INFO] Installation mode."
-        dry_run=0
-        ;;
-    *)
-        echo "[ERROR] Unknown argument: ${1}"
-        exit 1
-        ;;
-esac
+# shellcheck source=../install_init.sh
+. ../install_init.sh
 
 if [ "$(uname)" != "Darwin" ]; then
     echo "[SKIP] This script only works on macOS."
