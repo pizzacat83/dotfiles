7b617868de73d609fb68b7feff2ac70f535736b2
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-02-21 07:24:47 +0900

    download.sh: clone dotfiles repo

---
+++ download.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -eu
+set -eu
+
+if ! command -v git > /dev/null; then
+    # TODO: install git automatically?
+    echo "[ERROR] This script requires git installed."
+    exit 1
+fi
+
+dotfiles_dir="$HOME/dotfiles"
+
+git clone https://github.com/pizzacat83/dotfiles.git "$dotfiles_dir"
+
+echo "[INFO] completely downloaded dotfiles to $dotfiles_dir/dir"
