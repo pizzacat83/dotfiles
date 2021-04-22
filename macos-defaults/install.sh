022f4bb4e7b8b903940a29543a910010c5e65dff
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-04-22 19:12:46 +0900

    macos-defaults: remove incorrect schema check

--- macos-defaults/install.sh
+++ macos-defaults/install.sh
@@ -11,21 +11,12 @@ if [ "$(uname)" != "Darwin" ]; then
 fi
 
 # Set screenshot save location
-
-if defaults read com.apple.screencapture location > /dev/null; then
-    if [ "$dry_run" -eq 0 ]; then
-        screenshot_dir="$HOME/Pictures/screenshots"
-        if [ -d "$screenshot_dir" ]; then
-            mkdir -p "$screenshot_dir"
-        fi
-        defaults write com.apple.screencapture location "$screenshot_dir"
+if [ "$dry_run" -eq 0 ]; then
+    screenshot_dir="$HOME/Pictures/screenshots"
+    if [ -d "$screenshot_dir" ]; then
+        mkdir -p "$screenshot_dir"
     fi
-else
-    echo "[ERROR] Failed to set screenshot save location."
-    # shellcheck disable=SC2016
-    echo '`defaults read com.apple.screencapture location` failed.'
-    echo 'This could mean the config data structure has changed.'
-    exit 1
+    defaults write com.apple.screencapture location "$screenshot_dir"
 fi
 
 
