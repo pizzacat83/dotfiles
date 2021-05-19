bd32cda3f6070fa0d96b30cd82707744d51380c5
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-05-20 01:04:15 +0900

    update texlive to 2021

--- homedeploy/src/.config/fish/config.fish
+++ homedeploy/src/.config/fish/config.fish
@@ -12,8 +12,8 @@ set -x PATH "$HOME/.cargo/bin" $PATH
 # opam
 test -d ~/.opam && source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 # TeX Live
-set -x PATH /usr/local/texlive/2020/bin/x86_64-linux $PATH
-set -x PATH /usr/local/texlive/2020/bin/x86_64-darwin $PATH
+set -x PATH /usr/local/texlive/2021/bin/x86_64-linux $PATH
+set -x PATH /usr/local/texlive/2021/bin/universal-darwin/ $PATH
 # vscode
 set -x PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $PATH
 # gcloud (macOS)
