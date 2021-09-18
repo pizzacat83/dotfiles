1ef81422cb38161f5bbf98ff25ef9809b2ba6c79
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-09-18 10:51:11 +0900

    fish: add __zbind
    Signed-off-by: pizzacat83 <17941141+pizzacat83@users.noreply.github.com>

---
+++ homedeploy/src/.config/fish/functions/__zbind.fish
@@ -0,0 +1,22 @@
+# this function is based on PatrickF1/fzf.fish
+# Copyright © 2020 Patrick
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+function __zbind --description 'z command key binding'
+    set current_token (commandline --current-token)
+
+    set selected (
+        z -l |
+        string split -f 2 --no-empty ' ' |
+        fzf --query=$current_token --tiebreak=index
+    )
+
+    set new_token (string escape $selected)
+
+    commandline --current-token --replace $new_token
+
+    commandline --function repaint
+end
