4267c87241ffb8def55f62e26b83efe9812cfd8c
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2022-07-19 09:08:09 +0900

    brew: add setup scripts

--- package/brew/install.sh
+++ package/brew/install.sh
@@ -14,4 +14,16 @@ fi
 
 if [ "$dry_run" -eq 0 ]; then
     brew bundle install --file Brewfile.rb --no-lock
+
+    sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+    fish="$(which fish)"
+    if not grep -Fq "$fish" /etc/shells; then
+        echo "$fish" | sudo tee -a /etc/shells
+    fi
+    if echo 'Need to implement how to check whether fish is default shell'; false; then
+      # dscl . -read $HOME UserShell # for macos
+      printf "You will be asked for your password to change the default shell.\a"
+      chsh -s "$fish"
+    fi
 fi
